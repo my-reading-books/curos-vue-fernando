96bb0ab42cba435e25c84bfe41e837c95207cb43
diego-garro <diego.garromolina@yahoo.com>
2024-10-24 16:46:16 -0600

    section 5: class 60

--- src/composables/useChat.ts
+++ src/composables/useChat.ts
@@ -1,22 +1,45 @@
-import type { ChatMessage } from "@/interfaces/chat-message-interface"
-import { ref } from "vue"
+import { sleep } from '@/helpers/sleep';
+import type { ChatMessage } from '@/interfaces/chat-message-interface';
+import type { YesNoResponse } from '@/interfaces/yes-no.response';
+import { ref } from 'vue';
 
 export const useChat = () => {
-    const messages = ref<ChatMessage[]>([])
-      
-    const onMessage = (text: string) => {
-      messages.value.push({
-        id: new Date().getTime(),
-        itsMine: true,
-        message: text,
-      })
-    }
+  const messages = ref<ChatMessage[]>([]);
 
-    return {
-        // Properties
-        messages,
+  const getHerResponse = async () => {
+    const resp = await fetch('https://yesno.wtf/api');
+    const data = (await resp.json()) as YesNoResponse;
 
-        // Methods
-        onMessage,
-    }
-}
+    return data;
+  };
+
+  const onMessage = async (text: string) => {
+    if (text.length === 0) return;
+
+    messages.value.push({
+      id: new Date().getTime(),
+      itsMine: true,
+      message: text,
+    });
+
+    if (!text.endsWith('?')) return;
+
+    await sleep(1.5);
+    const { answer, image } = await getHerResponse();
+
+    messages.value.push({
+      id: new Date().getTime(),
+      itsMine: false,
+      message: answer,
+      image: image,
+    });
+  };
+
+  return {
+    // Properties
+    messages,
+
+    // Methods
+    onMessage,
+  };
+};
